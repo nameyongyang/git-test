c09fa48f0823c8fe1ead3f6ac3e4d8032bed1226
yangyong <294575885@qq.com>
2021-06-19 22:45:05 +0800

    first commit

---
+++ src/main/java/com/guigu/GitTest.java
@@ -0,0 +1,9 @@
+package com.guigu;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test1");
+        System.out.println("git test2");
+        System.out.println("git test3");
+    }
+}
