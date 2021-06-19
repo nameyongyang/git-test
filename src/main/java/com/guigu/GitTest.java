4c42010638c6c70783c44cc34c9492d8e73cb041
yangyong <294575885@qq.com>
2021-06-19 23:06:20 +0800

    hot-fix commit

--- src/main/java/com/guigu/GitTest.java
+++ src/main/java/com/guigu/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("git test3");
         System.out.println("git test4");
         System.out.println("git test5");
+        System.out.println("git test2_hot fix");
     }
 }
